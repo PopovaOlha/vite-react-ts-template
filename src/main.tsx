9ba9b83017724ac348bb627f81ab2de8e36f2b89
PopovaOlha <fc.odessa.10@gmail.com>
2024-07-23 15:18:18 +0300

    fix: theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider, useTheme } from './context/ThemeContext';
 import App from './components/App/App';
 import './index.css';
+
+const Root: React.FC = () => {
+  const { theme } = useTheme();
+
+  React.useEffect(() => {
+    document.body.className = theme === 'dark' ? 'dark' : 'light';
+  }, [theme]);
+
+  return <App />;
+};
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
@@ -12,7 +22,7 @@ if (rootElement) {
     <React.StrictMode>
       <Provider store={store}>
         <ThemeProvider>
-          <App />
+          <Root />
         </ThemeProvider>
       </Provider>
     </React.StrictMode>,
