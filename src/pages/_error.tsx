8d915f6037c0668b19641aed2f2a25d7f331b6c7
PopovaOlha <fc.odess.10@gmail.com>
2024-08-05 18:30:27 +0300

    feat: add error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import styles from './NotFound.module.css';
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
+import styles from '../styles/error.module.css';
 
 const NotFound: React.FC = () => {
   return (
     <div className={styles.container}>
       <div>
         <div className={styles.message}>404 - Page Not Found</div>
-        <Link to="/" className={styles.backLink}>
-          Go back to Home
+        <Link href="/">
+          <a className={styles.backLink}>Go back to Home</a>
         </Link>
       </div>
     </div>
